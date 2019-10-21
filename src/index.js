1cae79146f9bbd77be85a91a5e8a587005487ba0
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-10-21 11:30:58 -0700

    4 - ColorPicker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import '@clayui/css/lib/css/atlas.css';
 
 // Utils for color manipulation
 import {getRGB, rgbToHex} from './utils';
+import {ClaySelect} from '@clayui/form';
 import ClayButton from '@clayui/button';
 import ClayNavigationBar from '@clayui/navigation-bar';
 import React from 'react';
@@ -19,6 +20,7 @@ function App() {
 	const defaultPaletteNamesRef = React.useRef([]);
 
 	const [activePalette, setActivePalette] = React.useState(false);
+	const [activePart, setActivePart] = React.useState(PARTS[0].part);
 	const [palette, setPalette] = React.useState({});
 
 	React.useEffect(() => {
@@ -69,7 +71,31 @@ function App() {
 
 			<div className="container">
 				<div className="row align-items-center justify-content-center">
-					<Sofa {...palette} />
+					<div className="col-sm-7">
+						<Sofa {...palette} />
+					</div>
+					<div className="col-sm-4 sheet">
+						<h2 className="sheet-title">
+							Customize Sofa ({activePalette})
+						</h2>
+
+						<div className="sheet-section">
+							<label>
+								Sofa Part ({palette[`${activePart}Color`]})
+							</label>
+							<ClaySelect
+								onChange={e => setActivePart(e.target.value)}
+							>
+								{PARTS.map(item => (
+									<ClaySelect.Option
+										key={item.part}
+										label={item.label}
+										value={item.part}
+									/>
+								))}
+							</ClaySelect>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
